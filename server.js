af932507b346611ce0f9c5222b25782b17c2674f
Makeli Lima <107953987+kellydakine@users.noreply.github.com>
2022-09-08 00:55:26 -0300

    Correção de sintaxe

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-import app from "./src/app.js"
+import app from "./src/app.js";
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-  console.log(`Servidor escutando em http://localhost:${port}`)
-})
+  console.log(`Servidor escutando em http://localhost:${port}`);
+});
