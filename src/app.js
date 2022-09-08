6d24f6aff8dd7459df32702ec2b1dfec1f80ff33
Makeli Lima <107953987+kellydakine@users.noreply.github.com>
2022-09-08 00:56:19 -0300

    Add files via upload

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ import routes from "./routes/index.js";
 
 db.on("error", console.log.bind(console, "Erro de conexão"));
 db.once("open", () => {
-    console.log("Conexão com o banco feita com sucesso")
+    console.log("Conexão com o banco realizada com sucesso!");
 });
 
 const app = express();
@@ -12,4 +12,4 @@ app.use(express.json());
 
 routes(app);
 
-export default app
+export default app;
