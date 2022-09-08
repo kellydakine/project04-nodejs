43ca87e3942fc1e3cc4be8d717fd78fb7bd260bd
Makeli Lima <107953987+kellydakine@users.noreply.github.com>
2022-09-08 01:06:29 -0300

    Refatoração dos componentes e ajustes de status
    Ajustes de mensagens dos status das rotas

--- src/controllers/cadastrosController.js
+++ src/controllers/cadastrosController.js
@@ -2,8 +2,8 @@ import cadastros from "../models/Cadastro.js";
 
 class CadastroController {
     static listarCadastros = (req, res) => {
-        cadastros.find((err, cadastros) => {
-            const {page = 1, limit = 3} = req.query
+        cadastros.find((_, cadastros) => {
+            const {page = 1, limit = 3} = req.query;
             const startIndex = (page - 1) * limit;
             const endIndex = page * limit;
             const resultUsers = cadastros.slice(startIndex, endIndex);
@@ -13,57 +13,46 @@ class CadastroController {
 
     static findUserByName = (req, res) => {
         const name = req.query.name;
-
-        cadastros.find({'name': {$regex: name}}, {}, (err, cadastros) => {
-            if(err) {
-                res.status(404).send({message: `${err.message} - Usuário não encontrado.`});
-            } else {
-                res.status(200).send(cadastros);
-            }
+        cadastros.find({'name': {$regex: name, $options: 'i'}}, {}, (err, cadastros) => {
+            err 
+                ? res.status(404).send({message: `${err.message} - Usuário não encontrado!`})
+                : res.status(200).send(cadastros);
         }).select("-password");
     }
 
     static listarCadastrosId = (req, res) => {
         const id = req.params.id;
         cadastros.findById(id, (err, cadastros) => {
-            if(err) {
-                res.status(404).send({message: `${err.message} - ID do usuário não localizada!` })
-            } else {
-                res.status(200).send(cadastros);
-            }
+            err 
+                ? res.status(404).send({message: `ID do usuário não localizada!` })
+                : res.status(200).send(cadastros);
         }).select("-password");
     }
 
     static criarCadastro = (req, res) => {
         let newCadastro = new cadastros(req.body);
         newCadastro.save((err) => {
-            if(err) {
-                res.status(500).send({message: `${err.message} - Erro ao cadastrar novo usuário`});
-            } else {
-                res.status(201).send(newCadastro.toJSON());
-            }
+            err 
+                ? res.status(500).send({message: `Erro ao cadastrar novo usuário`})
+                : res.status(201).send(newCadastro.toJSON());
         });
     }
 
     static attCadastro = (req, res) => {
         const id = req.params.id;
         cadastros.findByIdAndUpdate(id, {$set: req.body}, (err) => {
-            if(!err) {
-                res.status(200).send({message: "O cadastro foi realizado com sucesso!"});
-            } else {
-                res.status(500).send({message: err.message});
-            }
+            !err 
+                ? res.status(200).send({message: "O cadastro foi realizado com sucesso!"})
+                : res.status(404).send({message: `Não foi possível localizar esse usuário!`});
         })
     }
 
     static excluirCadastro = (req, res) => {
         const id = req.params.id;
         cadastros.findByIdAndDelete(id, (err) => {
-            if(!err) {
-                res.status(200).send({message: "Usuário removido com sucesso!"});
-            } else {
-                res.status(404).send({message: "Usuário não encontrado!"});
-            }
+            !err
+                ? res.status(200).send({message: `Usuário removido com sucesso!`})
+                : res.status(404).send({message: "Usuário não encontrado, favor verificar novamente!"});
         })
     }
 }
