cf62d655817cb6ff1bcdcadb96acc0b1906b8118
Makeli Lima <107953987+kellydakine@users.noreply.github.com>
2022-09-07 23:28:28 -0300

    Adicionando a rota por busca de nome

--- src/routes/cadastrosRoutes.js
+++ src/routes/cadastrosRoutes.js
@@ -4,9 +4,10 @@ import CadastroController from "../controllers/cadastrosController.js";
 const router = express.Router();
 
 router
-    .get("/api/v1/users", CadastroController.listarCadastros)
+    .get("/api/v1/user", CadastroController.listarCadastros)
+    .get("/api/v1/user/busca", CadastroController.findUserByName)
     .get("/api/v1/user/:id", CadastroController.listarCadastrosId)
-    .post("/api/v1/users", CadastroController.criarCadastro)
+    .post("/api/v1/user", CadastroController.criarCadastro)
     .put("/api/v1/user/:id", CadastroController.attCadastro)
     .delete("/api/v1/user/:id", CadastroController.excluirCadastro)
 export default router;
